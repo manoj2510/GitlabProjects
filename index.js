a47dbdc5281ffa957419525d21273fa8e7c97f2d
NagarjunMallesh <ngarjunmallesh@gmail.com>
2023-08-15 02:58:54 -0400

    Update UI and Functionality

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // index.js
 
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
 
-const gitlabApiBaseUrl = 'https://git.cardiff.ac.uk/api/v4';
+const gitlabApiBaseUrl = "https://git.cardiff.ac.uk/api/v4";
 
-app.get('/api/projects', async (req, res) => {
+app.get("/api/projects", async (req, res) => {
   try {
-    const token = req.query.token || ''; // Get token from query parameter
-    const date = req.query.date || '';
+    const token = req.query.token || ""; // Get token from query parameter
+    const date = req.query.date || "";
 
     let url = `${gitlabApiBaseUrl}/projects?membership=true`;
 
     const response = await axios.get(url, {
       headers: {
-        'PRIVATE-TOKEN': token,
+        "PRIVATE-TOKEN": token,
       },
     });
 
@@ -27,7 +27,7 @@ app.get('/api/projects', async (req, res) => {
 
     // If a date is provided, filter projects based on created_at date
     if (date) {
-      projects = projects.filter(project => {
+      projects = projects.filter((project) => {
         const createdAt = new Date(project.created_at);
         const filterDate = new Date(date);
         return createdAt >= filterDate;
@@ -35,25 +35,28 @@ app.get('/api/projects', async (req, res) => {
     }
     res.json(projects);
   } catch (error) {
-    console.error('Error fetching projects:', error);
-    res.status(500).json({ error: 'Error fetching projects' });
+    console.error("Error fetching projects:", error);
+    res.status(500).json({ error: "Error fetching projects" });
   }
 });
 
-app.get('/api/commits/:projectId', async (req, res) => {
+app.get("/api/commits/:projectId", async (req, res) => {
   try {
     const projectId = req.params.projectId;
-    const response = await axios.get(`${gitlabApiBaseUrl}/projects/${projectId}/repository/commits`, {
-      headers: {
-        'PRIVATE-TOKEN': 'oY6duDBV4C6sEWxTS5Ei',
-      },
-    });
+    const response = await axios.get(
+      `${gitlabApiBaseUrl}/projects/${projectId}/repository/commits`,
+      {
+        headers: {
+          "PRIVATE-TOKEN": "oY6duDBV4C6sEWxTS5Ei",
+        },
+      }
+    );
 
     const commits = response.data;
     res.json(commits);
   } catch (error) {
-    console.error('Error fetching commits:', error);
-    res.status(500).json({ error: 'Error fetching commits' });
+    console.error("Error fetching commits:", error);
+    res.status(500).json({ error: "Error fetching commits" });
   }
 });
 
