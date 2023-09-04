bca974986575f9ae68ea93ed81873a53453f4578
NagarjunMallesh <ngarjunmallesh@gmail.com>
2023-09-03 23:43:19 -0400

    Updated Student Details

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get("/api/projects", async (req, res) => {
   }
 });
 
+//API call to retrieve the details of total commits in the project. to represent the contribution in bargraph
 app.get("/api/commits/:projectId", async (req, res) => {
   try {
     const projectId = req.params.projectId;
@@ -57,6 +58,68 @@ app.get("/api/commits/:projectId", async (req, res) => {
   } catch (error) {
     console.error("Error fetching commits:", error);
     res.status(500).json({ error: "Error fetching commits" });
+  }
+});
+
+// API to retrieve the student details for each project. To record contribution
+app.get("/api/student-details/:projectId", async (req, res) => {
+  try {
+    const projectId = req.params.projectId;
+
+    // Fetch commits for the project
+    const commitsResponse = await axios.get(
+      `${gitlabApiBaseUrl}/projects/${projectId}/repository/commits`,
+      {
+        headers: {
+          "PRIVATE-TOKEN": "oY6duDBV4C6sEWxTS5Ei",
+        },
+      }
+    );
+
+    const commits = commitsResponse.data;
+
+    // Process commits to get student details
+    const studentDetails = {};
+
+    commits.forEach((commit) => {
+      const authorName = commit.author_name;
+
+      // Initialize author if doesn't exist
+      if (!studentDetails[authorName]) {
+        studentDetails[authorName] = {
+          name: authorName,
+          totalCommits: 0,
+          firstCommitDate: null,
+          lastCommitDate: null,
+        };
+      }
+
+      // Increment total commits
+      studentDetails[authorName].totalCommits++;
+
+      // Update date range
+      const committedDate = new Date(commit.created_at);
+      if (
+        !studentDetails[authorName].firstCommitDate ||
+        committedDate < studentDetails[authorName].firstCommitDate
+      ) {
+        studentDetails[authorName].firstCommitDate = committedDate;
+      }
+
+      if (
+        !studentDetails[authorName].lastCommitDate ||
+        committedDate > studentDetails[authorName].lastCommitDate
+      ) {
+        studentDetails[authorName].lastCommitDate = committedDate;
+      }
+    });
+
+    // Convert to array
+    const studentDetailsArray = Object.values(studentDetails);
+    res.json(studentDetailsArray);
+  } catch (error) {
+    console.error("Error fetching student details:", error);
+    res.status(500).json({ error: "Error fetching student details" });
   }
 });
 
